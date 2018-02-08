e2ece55318a70e984705c7ae9ae1a6971d4a6352
Nikos Giannakis <n.giannakis@logicea.com>
2018-02-08 12:53:25 +0200

    WIP:initial implementation for GEMI

--- src/main/java/sample/Endpoint.java
+++ src/main/java/sample/Endpoint.java
@@ -1,5 +1,6 @@
 package sample;
 
+import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,7 +8,6 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -26,61 +26,66 @@ import java.util.Map;
 @Path("/hello")
 public class Endpoint {
 
-    @Autowired
-    private Cservice service;
+	@Autowired
+	private Cservice service;
 
-    @GET
-    @Path("/msg")
-    @Produces("application/json")
-    public Response message() {
-        Map<Object, Object> apiResponse = new HashMap<Object, Object>();
-        apiResponse.put("apiresponse", this.service.message());
-        return Response.ok(apiResponse).build();
+	@GET
+	@Path("/msg")
+	@Produces("application/json")
+	public Response message() {
+		Map<Object, Object> apiResponse = new HashMap<Object, Object>();
+		apiResponse.put("apiresponse", this.service.message());
+		return Response.ok(apiResponse).build();
 
-    }
-    @GET
-    @Path("/data")
-    @Produces("application/json")
-    public Response urlData() throws IOException {
-        Map<Object, Object> apiResponse = new HashMap<Object, Object>();
-        ArrayList<String> list=new ArrayList<String>();
-        ArrayList<Object> finalList=new ArrayList<Object>();
+	}
 
-        Document dc= Jsoup.connect("http://www.marktplaats.nl/z/auto-s/particulier-bedrijf.html?categoryId=91&attributes=S%2C10898&attributes=S%2C10899&startDateFrom=today").timeout(6000).get();
-        Elements element=dc.select(".cell-group.listing");
-        Element el1=null;
-        for (Element el : element) {
-            String linkText = el.select("span.mp-listing-priority-product").text();
-            if (!linkText.equals("Dagtopper")){
-                String url = el.select(".listing-title-description .heading>a").attr("href");
-                list.add(url);
+	@GET
+	@Path("/data")
+	@Produces("application/json")
+	public Response urlData() throws IOException {
+		Map<Object, Object> apiResponse = new HashMap<Object, Object>();
+		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<Object> finalList = new ArrayList<Object>();
 
-            }
-        }
-        for (String url : list) {
-            finalList.add(this.retData(url));
-           // break;
-        }
-        apiResponse.put("data",finalList);
-        //apiResponse.put("data1",el1);
+		Document dc = Jsoup.connect("https://www.businessregistry.gr/publicity/index").data("afm", "094263358")
+				.timeout(6000).post();
+		Elements links = dc.select("a[id^=a_]");
+		for (Element link : links) {
+			System.out.println(link);
+			Document newForm = Jsoup.connect("https://www.businessregistry.gr/publicity/show/" + "123").timeout(6000).post();
+		}
+		Elements element = dc.select("a[href]");
+		for (Element el : element) {
+			String linkText = el.select("span.mp-listing-priority-product").text();
+			if (!linkText.equals("Dagtopper")) {
+				String url = el.select(".listing-title-description .heading>a").attr("href");
+				list.add(url);
 
-        return Response.ok(apiResponse).build();
+			}
+		}
+		for (String url : list) {
+			finalList.add(this.retData(url));
+			// break;
+		}
+		apiResponse.put("data", finalList);
+		// apiResponse.put("data1",el1);
 
-    }
+		return Response.ok(apiResponse).build();
 
+	}
 
-    public Object retData(String url) throws IOException {
-        Map<Object, Object> resultData = new HashMap<Object, Object>();
-        Document dc= Jsoup.connect(url).get();
-        String viewCount=dc.select("#view-count").text();
-        String title=dc.select("#title").text();
-        String phone=dc.select(".phone-link.alternative").text();
-        resultData.put("view_count",viewCount);
-        resultData.put("title",title);
-        resultData.put("phone",phone);
+	public Object retData(String url) throws IOException {
+		Map<Object, Object> resultData = new HashMap<Object, Object>();
+		Document dc = Jsoup.connect(url).get();
+		String viewCount = dc.select("#view-count").text();
+		String title = dc.select("#title").text();
+		String phone = dc.select(".phone-link.alternative").text();
+		resultData.put("view_count", viewCount);
+		resultData.put("title", title);
+		resultData.put("phone", phone);
 
-        return resultData;
+		return resultData;
 
-    }
+	}
 
 }
